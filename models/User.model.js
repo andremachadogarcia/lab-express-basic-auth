071dfa49657fd246c606293e82d02b1460938012
“AndreGarcia <andre.m.garcia@icloud.com>
2021-03-27 00:22:16 -0300

    1
    1

--- models/User.model.js
+++ models/User.model.js
@@ -1,1 +1,12 @@
-// User model here
+const mongoose = requeire ('mongoose');
+
+const { Schema, model } = mongoose;
+
+const userSchema = new Schema ({
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+});
+
+const User = model('user', userSchema);
+
+module.exports = user;
